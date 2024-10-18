035c3905cb4f51d9e75515cfa69b638e8dc3d62d
Akash Chauhan <meachauhan@gmail.com>
2024-10-18 14:59:57 +0530

    GroovyDataTypes

---
+++ 04DataTypes.groovy
@@ -0,0 +1,11 @@
+int x=10;
+String name="Akash"
+long l=100
+float f=0.21f
+double d=0.21
+
+println("Integer x value is "+x)
+println("Long value is "+ l)
+println("Float value is "+ f)
+println("Double value is "+ d)
+println("String value is "+name)
