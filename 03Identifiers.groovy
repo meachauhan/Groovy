c46e71fd5824b1a068241146860ea5097518969d
Akash Chauhan <meachauhan@gmail.com>
2024-10-18 14:47:33 +0530

    Identifiers in Groovy

---
+++ 03Identifiers.groovy
@@ -0,0 +1,7 @@
+// def keyword is used to define identifiers like Number , String, characters,etc
+
+def x=10;
+def StudentName="Akash Chauhan"
+def characters='c'
+
+print("Integer "+x+" String value "+StudentName+" Character value "+characters)
