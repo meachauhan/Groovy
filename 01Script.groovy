dfc97390808b6b983786959bdf8bd6fda6dd697b
Akash Chauhan <meachauhan@gmail.com>
2024-10-18 14:39:09 +0530

    Introduction to Groovy

---
+++ 01Script.groovy
@@ -0,0 +1,6 @@
+// A Single line Comment
+/* A Multiline
+Comment*/
+// To run the script save the file with .groovy extension then in terminal type below;
+// groovy ScriptName.groovy
+print "Welcome to  Groovy Scripting"
