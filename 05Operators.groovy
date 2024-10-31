de5f88d5dd221ace3d7117b67d45cc580e93c003
Akash Chauhan <meachauhan@gmail.com>
2024-10-31 21:55:56 +0530

    Assignment Operator

---
+++ 05Operators.groovy
@@ -0,0 +1,10 @@
+class Operators{
+    static main(args) {
+        // AssignmentOperator
+        println("Addition= "+ 4+5)
+        println("Subtraction= "+5-2)
+        println("Multiplication= "+7*3)
+        println("Division= "+5/2)
+        
+    }
+}
