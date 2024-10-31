698c0d4c87ce10c3fcbdd0c0fce6a534b01e27bd
Akash Chauhan <meachauhan@gmail.com>
2024-10-31 22:07:06 +0530

    Range Operator

--- 05Operators.groovy
+++ 05Operators.groovy
@@ -5,6 +5,11 @@ class Operators{
         println("Subtraction= "+5-2)
         println("Multiplication= "+7*3)
         println("Division= "+5/2)
-        
+
+
+        //Range Operator
+        def range = 5..10; 
+        println(range); 
+        println(range.get(2)); 
     }
 }
